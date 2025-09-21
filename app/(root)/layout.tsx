17b5c8ca912fe568fe7553f1b9a7b89532c325f5
saptarshidey3000 <saptarshidey3000@gmail.com>
2025-09-21 20:13:27 +0530

    theme updated(customcss)

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "../../components/Navbar";
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <main className="font-work-sans min-h-screen bg-gradient-to-br from-gray-50 to-blue-50">
-      <Navbar />
+      <Navbar/>
       <div className="pt-6">{children}</div>
     </main>
   );
