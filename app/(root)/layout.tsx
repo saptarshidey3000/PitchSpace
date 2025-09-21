b1cdef476650c85544fce6bf4e52ff066e0f5f2e
saptarshidey3000 <saptarshidey3000@gmail.com>
2025-09-21 20:01:57 +0530

    navbar done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,10 @@
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar";
 
-export default function Layout({children}: Readonly<{children: React.ReactNode}>) {
-    return(
-        <main className="font-work-sans pt-17"> {/* pt-16 = height of navbar */}
-            <Navbar/>
-            {children}
-        </main>
-    )
+export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className="font-work-sans min-h-screen bg-gradient-to-br from-gray-50 to-blue-50">
+      <Navbar />
+      <div className="pt-6">{children}</div>
+    </main>
+  );
 }
